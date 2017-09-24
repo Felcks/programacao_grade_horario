aaa927aae7e3149d0aa9a47929ae82347cf3d35f
Matheus Felipe <matheusfelipecorreaalves@gmail.com>
2017-09-24 18:49:15 -0300

    Nova implementacao

--- Main.c
+++ Main.c
@@ -1,211 +1,150 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Fila.h"
-
-//fazer enumerado
-// 1 = matematica
-// 2 = portugues
-
-typedef struct disciplina{
-	int tipo;
-	int qtd_aulas;
-	int professor_alocado;
-
-} TurmaDisciplina;
-
-typedef struct turma{
-	//char nome[20];
-	int id;
-	int ano;
-	int qtd_disciplinas;
-	TurmaDisciplina* disciplinas;
-	int** aulas; 
-}Turma;
+#include <string.h>
 
 typedef struct professor{
 	int id;
-	int total_creditos;
-	int creditos_utilizados;
-	int disciplina;
-	int** aulas;
+	char nome[100];
 }Professor;
 
-typedef struct escola{
-	int dias_uteis;
-	int qtd_aulas;
-	int qtd_turmas;
-	Turma* turmas;
-	int qtd_professores;
-	Professor* professores;
-}Escola;
+typedef struct turma{
+	int id;
+	char nome[100];
+}Turma;
 
-void a(Escola* escola){
-	
+//Variaveis
+Professor* p; //Conjunto com todos os professores
+Turma* t; //Conjunto com todas as turmas
 
-	Professor* p = NULL;
-	for(int i = 0; i < escola->qtd_professores; i++){
-		if(p == NULL){
-			p = &escola->professores[i];
-		}
-		else{
-			if(escola->professores[i].total_creditos - escola->professores[i].creditos_utilizados > 
-				p->total_creditos - p->creditos_utilizados){
-				p = &escola->professores[i];
-			}
-		}
-	}
+int d[5] = {1, 2, 3, 4, 5}; //Conjunto de todos os dias de aula
+int nh = 5; //Número de horários de aula em um dia D
+int nd = 5; //Número de dias de aula por semana
 
-	Turma* turma_precisando = NULL;
-	TurmaDisciplina* td_precisando = NULL;
+int w[2][5][5]; //Não preferencia de um professor p por horário H no dia D --> w[p][h][d]
+int r[2][2]; //Número de aulas para a turma T que o professor p tem que atender --> r[p][t]
+int disp[2][5][5]; //Disponibilidade de um professor p num horário h num dia d --> disp[p][h][d]
+int ad[2][2]; //Número minímo de aulas duplas que um professor p da para uma turma t --> ad[p][t]
 
-	for(int i = 0; i < escola->qtd_turmas; i++){
-		
-		Turma* t = &escola->turmas[i];
-		for(int j = 0; j < t->qtd_disciplinas; j++){
-			TurmaDisciplina* tD = &t->disciplinas[j];
-			for(int k = 0; k < t->qtd_disciplinas; k++){
-				if(tD->tipo == p->disciplina){
-					if(tD->professor_alocado == 0 ||
-						tD->professor_alocado == p->id){
-						if(tD->qtd_aulas > 0){
-							turma_precisando = t;
-							td_precisando = tD;	
-						}	
-					}
-				}
-			}
-		}
-	}
+int as[2][2][5]; //1 Se o professor p da aula pra turma t no dia da semana d --> as[p][t][d]
+int ac[2][2][5][5]; //1 Se o professor p da aula pra turma t no dia d no horario h e h+1 --> ac[p][t][d][h]
+int am[2][5]; //1 Se o professor p tem aula marcada no dia d --> am[p][d]
+int b2[2][2][5]; //1 Se houver mais de uma aula (p, t) no dia da semana d, não consecutivas --> b2[p][t][d]
+int l[2][5]; //h Se for o ultimo horario  de aula agendado para o professor p no dia d, 0 caso contrario --> l[p][d]
+int f[2][5]; //h se h for o primeiro horario de aula agendado para o professor p no dia d, --> f[p][d]
+int b1[2][5]; //n se for o numero de aulas vagas entre duas aulas lecionadas por p no dia d --> b1[p][d]
+int d2[2][2]; //Numero de vezes que o requerimento de aulas em dias alternados para o par (p, t) não é atendido --> d2[p][t]
+int d3[2][2]; //Numero de vezes que o requerimento de aulas duplas para um par (p, t) não é atendido --> d3[p][t]
 
-	if(turma_precisando != NULL){
-		if(td_precisando != NULL){
-			td_precisando->professor_alocado = p->id;
-			for(int i = 0; i < escola->qtd_aulas; i++){
-				for(int j = 0; j < escola->dias_uteis; j++){
-					if(turma_precisando->aulas[i][j] == 0 && 
-						td_precisando->qtd_aulas > 0 && p->aulas[i][j] == 0){
-						turma_precisando->aulas[i][j] = p->disciplina;
-						td_precisando->qtd_aulas--;
-						p->creditos_utilizados++;
-						p->aulas[i][j] = turma_precisando->id;
-						break;
-					}					
-				}
-			}
-		}
-	}
+int x[2][2][5][5]; //Variavel de decisao! 1 se o professor p da aula pra turma t no horario h e dia d --> x[p][t][h][d]
 
 
+void criarProfessores(Professor* p){
 
-	
+	p = malloc(2 * sizeof(Professor));
+	p[0].id = 0;
+	strcpy(p[0].nome, "Marcelo");
 
+	p[1].id = 1;
+	strcpy(p[1].nome, "João");
 }
 
+void criarTurmas(Turma* t){
 
+	t = malloc(2 * sizeof(Turma));
+	t[0].id = 0;
+	strcpy(t[0].nome, "1701");
+
+	t[1].id = 1;
+	strcpy(t[1].nome, "1702");
+}
 
 
 int main(void){
 
-	char url[]="Entrada.txt";
-	char ch;
-	char aux[100];
-	FILE *arq;
+	criarProfessores(p);
+	criarTurmas(t);
 
-	Escola* escola = (Escola*)malloc(sizeof(Escola));
-
-	arq = fopen(url, "r");
-	if(arq == NULL)
-	    printf("Erro, nao foi possivel abrir o arquivo\n");
-	else{
-
-	    fscanf(arq, "%s %i", aux, &escola->dias_uteis);
-	    fscanf(arq, "%s %i", aux, &escola->qtd_aulas);
-
-	    fscanf(arq, "%s %i", aux, &escola->qtd_turmas);
-	    escola->turmas = (Turma*)malloc(escola->qtd_turmas * sizeof(Turma));
-	    for(int i = 0; i < escola->qtd_turmas; i++){
-
-	    	Turma* t = &escola->turmas[i];
-
-	    	fscanf(arq, "%i %i", &t->id, &t->qtd_disciplinas);
-	    	t->disciplinas = (TurmaDisciplina*)malloc(t->qtd_disciplinas * 
-	    											sizeof(TurmaDisciplina));
-
-	    	for(int j = 0; j < t->qtd_disciplinas; j++){
-	    		TurmaDisciplina* d = &t->disciplinas[j];
-	    		fscanf(arq, "%i %i ", &d->tipo, &d->qtd_aulas);
-	    		d->professor_alocado = 0;
-	    	}
-
-	    	t->aulas = (int**)malloc(escola->qtd_aulas * sizeof(int*));
-	    	for(int j = 0; j < escola->qtd_aulas; j++){
-
-	    		t->aulas[j] = (int*)malloc(escola->dias_uteis * sizeof(int));
-	    		for(int k = 0; k < escola->dias_uteis; k++){
-	    			t->aulas[j][k] = 0;
-	    		}
-	    	}
-	    }
-
-	    fscanf(arq, "%s %i", aux, &escola->qtd_professores);
-	    escola->professores = (Professor*)malloc(escola->qtd_professores * sizeof(Professor));
-	    for(int i = 0; i < escola->qtd_professores; i++){
-	    	Professor* p = &escola->professores[i];
-	    	fscanf(arq, "%i %i %i", &p->id, &p->disciplina, &p->total_creditos);
-	    	p->creditos_utilizados = 0;
-	    	p->aulas = (int**)malloc(escola->qtd_aulas * sizeof(int*));
-	    	for(int j = 0; j < escola->qtd_aulas; j++){
-
-	    		p->aulas[j] = (int*)malloc(escola->dias_uteis * sizeof(int));
-	    		for(int k = 0; k < escola->dias_uteis; k++){
-	    			p->aulas[j][k] = 0;
-	    		}
-	    	}
-
-	    }
+	//setando as aulas pra cada turma
+	r[0][0] = 6;
+	r[0][1] = 4;
+	r[1][0] = 4;
+	r[1][1] = 6;
 
 
+	
 
-	    a(escola);
-	    a(escola);
-	    a(escola);
-	    a(escola);
+	int pu = 0;
+	int tu = 0;
+	float maiorUrgencia = 0;
+
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++){
 
 
+			if(r[i][j] > 0){ //Se aquele professor da aula pra turma
+				
+				float urgencia = 0;
+				urgencia = r[i][j];
 
-	    /*
 
-	    fscanf(arq, "%s %i", aux, &qtd_tarefas);
-	    for(int i = 0; i < qtd_tarefas; i++){
-	    	fscanf(arq, "%s %i", aux, &tempo_tarefas[i]);
-	    }
+				int divider = 1;
+				for(int  k = 0; k < 5; k++){
+					for(int y = 0; y < 5; y++){
+						if(x[i][j][k][y] == 0){
+							divider += 1;
+						}
+					}
+				}
 
-	    //while( (fscanf(arq,"%s %f %f %f\n", nome, &nota1, &nota2, &nota3))!=EOF )
-	    //while( (ch=fgetc(arq))!= EOF )
-		//putchar(ch);*/
-	}
-
-	//Printando saída
-	for(int i = 0; i < escola->qtd_turmas; i++){
-		Turma* t = &escola->turmas[i];
-		printf("Turma %i\n", t->id);
-		for(int j = 0; j < escola->qtd_aulas; j++){
-			for(int k = 0; k < escola->dias_uteis; k++){
-				printf("%i ", t->aulas[j][k]);
+				urgencia = urgencia / divider;
+				if(urgencia > maiorUrgencia){
+					maiorUrgencia = urgencia;
+					pu = j;
+					tu = i;
+				}
 			}
-			printf("\n");
+		}
+	}
+	
+	int alocou = 0;
+	for(int  k = 0; k < 5; k++){
+		if(alocou == 1)
+			break;
+		for(int y = 0; y < 5; y++){
+			if(x[pu][tu][k][y] == 0){
+				x[pu][tu][k][y] = 1;
+				r[pu][tu] -= 1;
+				alocou = 1;
+
+
+				if(x[pu][tu][k+1][y] == 0){
+					x[pu][tu][k+1][y] = 1;
+					r[pu][tu] -= 1;
+				}
+				break;
+			}
 		}
 	}
 
-	//Construção inicial
-	//Pseudo urgencia (56)
-	//Pega professor com mais créditos sobrando
-	//Pega primeira turma que precisa dessa matéria e ele é o professor
-	// 		ou não tem professor alocado
-	//Checa se a turma já teve duas aulas naquele dia com aquele professor
-	
-
-	fclose(arq);
+	//Printando a variavel de decisão
+	for(int i = 0; i < 2; i++){
+		printf("Professor %i\n",i);
+		for(int j = 0; j < 2; j++){
+			printf("Turma %i\n", t);
+			printf("Tem que dar: %i \n", r[i][j]);
+			int qtd_aulas = 0;
+			printf("               dias       \n");
+			for(int  k = 0; k < 5; k++){
+				printf("horario %i = ", k);
+				for(int y = 0; y < 5; y++){
+					printf("%i ", x[i][j][k][y]);
+					qtd_aulas += x[i][j][k][y];
+				}
+				printf("\n");
+			}
+			printf("atualmente dando: %i\n\n", qtd_aulas);
+		}
+	}
 
 
 }
